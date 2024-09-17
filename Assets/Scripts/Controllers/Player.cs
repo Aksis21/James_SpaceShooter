2757ac99c6c3e49c4127a1e722be9b8fdd905a27
Joshua <james22@sheridancollege.ca>
2024-09-17 14:10:41 -0400

    Player movement added
    Jeff movement script added.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,10 +8,31 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    float moveSpeed = 0.5f;
 
     void Update()
     {
 
     }
 
+    private void FixedUpdate()
+    {
+
+        Vector3 offset = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            offset += Vector3.left * moveSpeed;
+        if (Input.GetKey(KeyCode.UpArrow))
+            offset += Vector3.up * moveSpeed;
+        if (Input.GetKey(KeyCode.DownArrow))
+            offset += Vector3.down * moveSpeed;
+        if (Input.GetKey(KeyCode.RightArrow))
+            offset += Vector3.right * moveSpeed;
+
+        PlayerMovement(offset);
+    }
+
+    void PlayerMovement(Vector3 offset)
+    {
+        transform.position += offset;
+    }
 }
