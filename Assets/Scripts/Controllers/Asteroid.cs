f15609f01021f4b15aa9aa6f2d4da29842e7e47e
Aksis21 <james22@sheridancollege.ca>
2024-09-23 14:39:08 -0400

    Task 3 complete.
    Task 3 working as intended, although it does make the game screen messy rather quickly.

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -7,14 +7,23 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+    Vector3 destination = Vector3.zero;
+    Vector3 distance;
+    bool newDestination = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        if (newDestination)
+        {
+            destination.x = transform.position.x + Random.Range(-maxFloatDistance, maxFloatDistance);
+            destination.y = transform.position.y + Random.Range(-maxFloatDistance, maxFloatDistance);
+            newDestination = false;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed);
+
+        distance = transform.position - destination;
+        if (distance.magnitude < arrivalDistance)
+            newDestination = true;
     }
 }
