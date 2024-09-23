e94da2e228da12874e49ab267d28b35428c9fc19
Aksis21 <james22@sheridancollege.ca>
2024-09-23 15:17:33 -0400

    Task 4 complete.
    Task 4 completed with debug drawlines taking 1 second to draw between each star, regardless of distance.

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -6,9 +6,28 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    Vector3 linePosition;
+    int i = 0;
+    float drawSpeed;
 
-    // Update is called once per frame
+    void Start()
+    {
+        linePosition = starTransforms[0].position;
+        drawSpeed = Vector3.Distance(linePosition, starTransforms[i + 1].position) / drawingTime;
+    }
+
     void Update()
     {
+
+        linePosition = Vector3.MoveTowards(linePosition, starTransforms[i+1].position, drawSpeed * Time.deltaTime);
+        Debug.DrawLine(starTransforms[i].position, linePosition, Color.white);
+        if (linePosition == starTransforms[i+1].position)
+        {
+            if (i == starTransforms.Count - 2)
+                i = 0;
+            else
+                i += 1;
+            drawSpeed = Vector3.Distance(linePosition, starTransforms[i + 1].position) / drawingTime;
+        }
     }
 }
