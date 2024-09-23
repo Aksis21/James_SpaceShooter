c8d29fe5e7110f456cc5dd123207ad552991c47d
Aksis21 <james22@sheridancollege.ca>
2024-09-23 14:28:04 -0400

    Tasks 1 and 2 complete.
    Task 1b and 2b successfully completed, task 1c unable to complete in time.

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
+    public List<Transform> asteroidTransform;
+    bool newTarget = true;
+    Transform targetLoc;
+    public float moveSpeed = 1f;
+    Vector3 distance;
+
     private void Update()
     {
+        int targetCount = asteroidTransform.Count;
+        
+        if (newTarget == true)
+        {
+            int targetNum = Random.Range(0, targetCount);
+            targetLoc = asteroidTransform[targetNum];
+            newTarget = false;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetLoc.position, moveSpeed * Time.deltaTime);
+        distance = transform.position - targetLoc.position;
+        if (distance.magnitude < 1)
+        {
+            newTarget = true;
+        }
     }
 
 }
--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,10 +8,21 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
-    public float moveSpeed = 10.0f;
+    public float maxSpeed = 10.0f;
+    public float accelerationTime = 1f;
+    float moveSpeed = 0f;
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveSpeed += maxSpeed/accelerationTime * Time.deltaTime;
+        }
+        else
+        {
+            moveSpeed = 0f;
+        }
+
         Vector3 offset = Vector3.zero;
         if (Input.GetKey(KeyCode.LeftArrow))
             offset += Vector3.left * moveSpeed;
@@ -21,6 +32,8 @@ public class Player : MonoBehaviour
             offset += Vector3.down * moveSpeed;
         if (Input.GetKey(KeyCode.RightArrow))
             offset += Vector3.right * moveSpeed;
+
+        offset = offset.normalized * Mathf.Clamp(offset.magnitude, 0, maxSpeed);
 
         PlayerMovement(offset);
     }
