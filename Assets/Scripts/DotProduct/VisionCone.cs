16244caf2994d9536632010686d3e93cefe87912
Aksis21 <james22@sheridancollege.ca>
2024-10-08 14:32:45 -0400

    Dotproduct exercise.
    Full dotproduct exercise with vision cone complete.

---
+++ Assets/Scripts/DotProduct/VisionCone.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionCone : MonoBehaviour
+{
+    public GameObject target;
+
+    public float sightDistance;
+    public float visionAngle;
+
+    void Update()
+    {
+        float angleInRads = visionAngle * Mathf.Deg2Rad;
+
+        Vector3 leftVector = new Vector3(Mathf.Cos(angleInRads), Mathf.Sin(angleInRads), 0) * sightDistance;
+        Vector3 rightVector = new Vector3(Mathf.Cos(-angleInRads), Mathf.Sin(-angleInRads), 0) * sightDistance;
+
+        Debug.DrawLine(transform.position, transform.position + leftVector, Color.cyan);
+        Debug.DrawLine(transform.position, transform.position + rightVector, Color.cyan);
+
+        if (target != null)
+        {
+            Vector3 vectorToTarget = target.transform.position - transform.position;
+
+            Debug.DrawLine(transform.position, transform.position + vectorToTarget, Color.green);
+
+            float targetDotProduct = Vector3.Dot(transform.right, vectorToTarget.normalized);
+            float visionDotProduct = Vector3.Dot(transform.right, leftVector.normalized);
+
+            if (targetDotProduct >= visionDotProduct && vectorToTarget.magnitude <= sightDistance)
+            {
+                print("<color=orange><size=18>Target spotted!</size></color>");
+            }
+        }
+    }
+}
